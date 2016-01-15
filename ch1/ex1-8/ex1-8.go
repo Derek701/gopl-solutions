b021facd21003f8035bb385871225357e3f7ed21
Derek Miller <Derek701@users.noreply.github.com>
2016-01-15 12:49:06 -0600

    Complete Exercise 1.8
    Modify fetch to add the prefix http:// to each argument URL if it is
    missing.

--- ch1/ex1-8/ex1-8.go
+++ ch1/ex1-8/ex1-8.go
@@ -6,10 +6,14 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
