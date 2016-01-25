600064caef9aafed4cea1b1ab982fbcd5aa2fec0
Derek Miller <Derek701@users.noreply.github.com>
2016-01-25 13:59:30 -0600

    Complete Exercise 2.4
    Write a version of PopCount that counts bits by shifting its argument
    through 64 bit positions, testing the rightmost bit each time, and
    compare its performance to the table-lookup versions.

---
+++ ch2/ex2-4/ex2-4.go
@@ -0,0 +1,12 @@
+package popcount
+
+// PopCountByShifting returns the population count (number of set bits) of x.
+func PopCountByShifting(x uint64) int {
+	n := 0
+	for i := uint(0); i < 64; i++ {
+		if x&(1<<i) != 0 {
+			n++
+		}
+	}
+	return n
+}
---
+++ ch2/ex2-4/ex2-4_test.go
@@ -0,0 +1,33 @@
+package popcount
+
+import (
+	"testing"
+
+	"github.com/Derek701/gopl-solutions/ch2/ex2-3/popcount"
+)
+
+// -- Benchmarks --
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCount(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCount2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		popcount.PopCount2(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountByShifting(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByShifting(0x1234567890ABCDEF)
+	}
+}
+
+// Intel Core i5-4200U @ 1.60GHz
+// $ go test -bench=.
+// BenchmarkPopCount-4                  200000000         6.97 ns/op
+// BenchmarkPopCount2-4                 100000000        18.2 ns/op
+// BenchmarkPopCountByShifting			20000000        118 ns/op
