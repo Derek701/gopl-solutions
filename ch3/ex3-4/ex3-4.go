192faf85d7d570256012e4449822ebbd299ed719
Derek Miller <Derek701@users.noreply.github.com>
2016-01-27 15:31:28 -0600

    Complete Exercise 3.4
    Construct a web server for the surface program and allow the client to
    specify height, width, and color as HTTP request parameters.

--- ch3/ex3-4/ex3-4.go
+++ ch3/ex3-4/ex3-4.go
@@ -3,35 +3,64 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"math"
+	"net/http"
+	"strconv"
 )
 
 const (
-	width, height = 600, 320            // canvas size in pixels
-	cells         = 100                 // number of grid cells
-	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
-	xyscale       = width / 2 / xyrange // pixels per x or y unit
-	zscale        = height * 0.4        // pixels per z unit
-	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+	cells   = 100         // number of grid cells
+	xyrange = 30.0        // axis ranges (-xyrange..+xyrange)
+	angle   = math.Pi / 6 // angle of x, y axes (=30°)
 )
 
+var width, height int
+var xyscale, zscale float64
+var color string
 var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
-		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
-		"width='%d' height='%d'>", width, height)
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/svg+xml")
+	width, height = 600, 320               // canvas size in pixels
+	xyscale = float64(width) / 2 / xyrange // pixels per x or y unit
+	zscale = float64(height) * 0.4         // pixels per z unit
+	color = "white"                        // fill color of polygon
+	if r.FormValue("height") != "" {
+		height, _ = strconv.Atoi(r.FormValue("height"))
+		zscale = float64(height) * 0.4
+	}
+	if r.FormValue("width") != "" {
+		width, _ = strconv.Atoi(r.FormValue("width"))
+		xyscale = float64(width) / 2 / xyrange
+	}
+	if r.FormValue("color") != "" {
+		color = r.FormValue("color")
+	}
+	surface(w)
+}
+
+func surface(out io.Writer) {
+	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
+		"style='stroke: grey; fill: %s; stroke-width: 0.7' "+
+		"width='%d' height='%d'>", color, width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
 			ax, ay := corner(i+1, j)
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("</svg>")
+	fmt.Fprintln(out, "</svg>")
 }
 
 func corner(i, j int) (float64, float64) {
@@ -43,8 +72,8 @@ func corner(i, j int) (float64, float64) {
 	z := f(x, y)
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
-	sx := width/2 + (x-y)*cos30*xyscale
-	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
+	sx := float64(width)/2 + (x-y)*cos30*xyscale
+	sy := float64(height)/2 + (x+y)*sin30*xyscale - z*zscale
 	return sx, sy
 }
 
