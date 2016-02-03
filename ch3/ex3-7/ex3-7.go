0cf607f3a778edc31c9e033bfbb0bdb480fd3790
Derek Miller <Derek701@users.noreply.github.com>
2016-02-03 11:51:47 -0600

    Change fractal
    Replace the Mandelbrot set with a simple fractal using Newton's method.

--- ch3/ex3-7/ex3-7.go
+++ ch3/ex3-7/ex3-7.go
@@ -1,4 +1,4 @@
-// Mandelbrot emits a PNG image of the Mandelbrot fractal.
+// Newton emits a PNG image of a simple fractal using Newton's method.
 package main
 
 import (
@@ -22,21 +22,24 @@ func main() {
 			x := float64(px)/width*(xmax-xmin) + xmin
 			z := complex(x, y)
 			// Image point (px, py) represents complex value z.
-			img.Set(px, py, mandelbrot(z))
+			img.Set(px, py, newton(z))
 		}
 	}
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
 }
 
-func mandelbrot(z complex128) color.Color {
-	const iterations = 200
-	const contrast = 15
-
-	var v complex128
-	for n := uint8(0); n < iterations; n++ {
-		v = v*v + z
-		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+// f(x) = x^4 - 1
+//
+// z' = z - f(z)/f'(z)
+//    = z - (z^4 - 1) / (4 * z^3)
+//    = z - (z - 1/z^3) / 4
+func newton(z complex128) color.Color {
+	const iterations = 37
+	const contrast = 7
+	for i := uint8(0); i < iterations; i++ {
+		z -= (z - 1/(z*z*z)) / 4
+		if cmplx.Abs(z*z*z*z-1) < 1e-6 {
+			return color.Gray{255 - contrast*i}
 		}
 	}
 	return color.Black
