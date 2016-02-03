1e495c9c5d2f69ec1d2cbc615a9ec64fb2c817bf
Derek Miller <Derek701@users.noreply.github.com>
2016-02-03 10:41:48 -0600

    Complete Exercise 3.6
    Implement a supersampling technique to reduce the effect of pixelation
    by computing the color value at several points and taking the average.

--- ch3/ex3-6/ex3-6.go
+++ ch3/ex3-6/ex3-6.go
@@ -18,17 +18,25 @@ func main() {
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
 	for py := 0; py < height; py++ {
 		y := float64(py)/height*(ymax-ymin) + ymin
+		ys := float64(py+1)/height*(ymax-ymin) + ymin
 		for px := 0; px < width; px++ {
 			x := float64(px)/width*(xmax-xmin) + xmin
-			z := complex(x, y)
+			xs := float64(px+1)/width*(xmax-xmin) + xmin
+			z := [4]complex128{ // Supersample four points.
+				complex(x, y), complex(x, ys), complex(xs, y), complex(xs, ys)}
 			// Image point (px, py) represents complex value z.
-			img.Set(px, py, mandelbrot(z))
+			var ss int
+			for i := range z {
+				ss += int(mandelbrot(z[i]))
+			}
+			ss /= 4
+			img.Set(px, py, color.Gray{uint8(ss)})
 		}
 	}
 	png.Encode(os.Stdout, img) // NOTE: ignoring errors
 }
 
-func mandelbrot(z complex128) color.Color {
+func mandelbrot(z complex128) uint8 {
 	const iterations = 200
 	const contrast = 15
 
@@ -36,8 +44,8 @@ func mandelbrot(z complex128) color.Color {
 	for n := uint8(0); n < iterations; n++ {
 		v = v*v + z
 		if cmplx.Abs(v) > 2 {
-			return color.Gray{255 - contrast*n}
+			return 255 - contrast*n
 		}
 	}
-	return color.Black
+	return 0 // Black
 }
