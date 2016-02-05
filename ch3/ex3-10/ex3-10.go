87113d22fcd3a06153946b10837dd213f9899d2d
Derek Miller <Derek701@users.noreply.github.com>
2016-02-05 15:49:21 -0600

    Complete Exercise 3.10
    Write a non-recursive version of comma, using bytes instead of string
    concatenation.

--- ch3/ex3-10/ex3-10.go
+++ ch3/ex3-10/ex3-10.go
@@ -2,6 +2,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"os"
 )
@@ -14,9 +15,21 @@ func main() {
 
 // comma inserts commas in a non-negative decimal integer string.
 func comma(s string) string {
+	var buf bytes.Buffer
 	n := len(s)
 	if n <= 3 {
 		return s
 	}
-	return comma(s[:n-3]) + "," + s[n-3:]
+	commaPos := n % 3
+	if commaPos == 0 {
+		commaPos = 3
+	}
+	for i := 0; i < n; i++ {
+		if i == commaPos {
+			buf.WriteByte(',')
+			commaPos += 3
+		}
+		buf.WriteByte(s[i])
+	}
+	return buf.String()
 }
