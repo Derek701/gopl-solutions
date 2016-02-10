505339e4e30ccdfd412052b915d3e40ec369e83b
Derek Miller <Derek701@users.noreply.github.com>
2016-02-10 14:15:44 -0600

    Complete Exercise 3.12
    Write a function that reports whether two strings are anagrams of each
    other, that is, they contain the same letters in a different order.

---
+++ ch3/ex3-12/anagram/ex3-12.go
@@ -0,0 +1,29 @@
+// Package anagram provides a function to check for anagrams.
+package anagram
+
+import (
+	"reflect"
+	"strings"
+	"unicode"
+)
+
+// IsAnagram reports whether two strings are anagrams of each other.
+func IsAnagram(s1, s2 string) bool {
+	s1, s2 = strings.TrimSpace(s1), strings.TrimSpace(s2)
+	if s1 == s2 {
+		return false // A string is not an anagram of itself.
+	}
+	counts1 := make(map[rune]int)
+	for _, r := range s1 {
+		if unicode.IsLetter(r) {
+			counts1[r]++
+		}
+	}
+	counts2 := make(map[rune]int)
+	for _, r := range s2 {
+		if unicode.IsLetter(r) {
+			counts2[r]++
+		}
+	}
+	return reflect.DeepEqual(counts1, counts2)
+}
