19bd866e5e40672d162f8d5d81de2cdfdc720650
Derek Miller <Derek701@users.noreply.github.com>
2016-02-10 17:00:38 -0600

    Export all declarations

--- ch3/ex3-13/ex3-13.go
+++ ch3/ex3-13/ex3-13.go
@@ -1,12 +1,12 @@
 // Code snippet of compact const declarations for kB through YB.
 
 const (
-	kB = 1000    // kilobyte  = 1000
-	MB = kB * kB // megabyte  = 1000²
-	GB = MB * kB // gigabyte  = 1000³
-	TB = GB * kB // terabyte  = 1000⁴ (exceeds 1 << 32)
-	PB = TB * kB // petabyte  = 1000⁵
-	EB = PB * kB // exabyte   = 1000⁶
-	ZB = EB * kB // zettabyte = 1000⁷ (exceeds 1 << 64)
-	YB = ZB * kB // yottabyte = 1000⁸
+	KB = 1000    // kilobyte  = 1000
+	MB = KB * KB // megabyte  = 1000²
+	GB = MB * KB // gigabyte  = 1000³
+	TB = GB * KB // terabyte  = 1000⁴ (exceeds 1 << 32)
+	PB = TB * KB // petabyte  = 1000⁵
+	EB = PB * KB // exabyte   = 1000⁶
+	ZB = EB * KB // zettabyte = 1000⁷ (exceeds 1 << 64)
+	YB = ZB * KB // yottabyte = 1000⁸
 )
