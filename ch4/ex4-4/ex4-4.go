352e41fc0bc2fe42d6448229c112ac1a3f0acb1e
Derek Miller <Derek701@users.noreply.github.com>
2016-02-25 14:42:57 -0600

    Begin Exercise 4.4
    Write a version of rotate that operates in three passes.

--- ch4/ex4-4/ex4-4.go
+++ ch4/ex4-4/ex4-4.go
@@ -5,9 +5,7 @@ import "fmt"
 func main() {
 	s := []int{0, 1, 2, 3, 4, 5}
 	// Rotate s left by two positions.
-	reverse(s[:2])
-	reverse(s[2:])
-	reverse(s)
+	rotate(s[:], 2, false)
 	fmt.Println(s) // "[2 3 4 5 0 1]"
 }
 
@@ -15,5 +13,17 @@ func main() {
 func reverse(s []int) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
+	}
+}
+
+// rotate rotates a slice of ints in place by p elements to the right or left.
+func rotate(s []int, p int, r bool) {
+	if r {
+		reverse(s)
+	}
+	reverse(s[:p])
+	reverse(s[p:])
+	if !r {
+		reverse(s)
 	}
 }
