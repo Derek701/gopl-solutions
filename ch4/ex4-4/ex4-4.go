595a92dc53591dbf6b1370761df9feaf1138bdb1
Derek Miller <Derek701@users.noreply.github.com>
2016-02-25 14:26:31 -0600

    Add rotate

---
+++ ch4/ex4-4/ex4-4.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{0, 1, 2, 3, 4, 5}
+	// Rotate s left by two positions.
+	reverse(s[:2])
+	reverse(s[2:])
+	reverse(s)
+	fmt.Println(s) // "[2 3 4 5 0 1]"
+}
+
+// reverse reverses a slice of ints in place.
+func reverse(s []int) {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
+	}
+}
