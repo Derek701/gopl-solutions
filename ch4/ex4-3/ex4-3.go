621fdc3b81ae0d166d8cd0fe7c8dcc256164c088
Derek Miller <Derek701@users.noreply.github.com>
2016-02-25 14:18:27 -0600

    Complete Exercise 4.3
    Rewrite reverse to use an array pointer instead of a slice.

--- ch4/ex4-3/ex4-3.go
+++ ch4/ex4-3/ex4-3.go
@@ -2,15 +2,17 @@ package main
 
 import "fmt"
 
+const aLen = 6 // Define the array size for function reverse.
+
 func main() {
 	a := [...]int{0, 1, 2, 3, 4, 5}
-	reverse(a[:])
+	reverse(&a)
 	fmt.Println(a) // "[5 4 3 2 1 0]"
 }
 
-// reverse reverses a slice of ints in place.
-func reverse(s []int) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
+// reverse reverses an array of ints in place.
+func reverse(aPtr *[aLen]int) {
+	for i, j := 0, len(aPtr)-1; i < j; i, j = i+1, j-1 {
+		aPtr[i], aPtr[j] = aPtr[j], aPtr[i]
 	}
 }
