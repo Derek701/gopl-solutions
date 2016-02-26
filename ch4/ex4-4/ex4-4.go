76501199058563c195a1887076c98a653a8fa88b
Derek Miller <Derek701@users.noreply.github.com>
2016-02-25 21:50:39 -0600

    Update rotate parameters and logic
    Use negative values to denote left rotations.

--- ch4/ex4-4/ex4-4.go
+++ ch4/ex4-4/ex4-4.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	s := []int{0, 1, 2, 3, 4, 5}
 	// Rotate s left by two positions.
-	rotate(s[:], 2, false)
+	rotate(s[:], -2)
 	fmt.Println(s) // "[2 3 4 5 0 1]"
 }
 
@@ -17,17 +17,15 @@ func reverse(s []int) {
 }
 
 // rotate rotates a slice of ints in place by p elements to the right or left.
-func rotate(s []int, p int, r bool) {
+func rotate(s []int, p int) {
 	if len(s) == 0 || p == 0 { // check for no rotation
 		return
 	}
 	p %= len(s) // skip unnecessary complete rotations and keep p in-bounds
-	if r {
-		reverse(s)
+	if p < 0 {
+		p += len(s) // convert left rotation to the equivalent right rotation
 	}
+	reverse(s)
 	reverse(s[:p])
 	reverse(s[p:])
-	if !r {
-		reverse(s)
-	}
 }
