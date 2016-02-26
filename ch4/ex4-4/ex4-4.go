6065a7642356c058704def98fbabde7d0926eff2
Derek Miller <Derek701@users.noreply.github.com>
2016-02-25 22:35:37 -0600

    Complete Exercise 4.4
    Write a version of rotate that operates in a single pass.

--- ch4/ex4-4/ex4-4.go
+++ ch4/ex4-4/ex4-4.go
@@ -9,13 +9,6 @@ func main() {
 	fmt.Println(s) // "[2 3 4 5 0 1]"
 }
 
-// reverse reverses a slice of ints in place.
-func reverse(s []int) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-}
-
 // rotate rotates a slice of ints in place by p elements to the right or left.
 func rotate(s []int, p int) {
 	if len(s) == 0 || p == 0 { // check for no rotation
@@ -25,7 +18,8 @@ func rotate(s []int, p int) {
 	if p < 0 {
 		p += len(s) // convert left rotation to the equivalent right rotation
 	}
-	reverse(s)
-	reverse(s[:p])
-	reverse(s[p:])
+	tmp := make([]int, len(s))
+	copy(tmp, s[len(s)-p:])
+	copy(tmp[p:], s[:len(s)-p])
+	copy(s, tmp)
 }
