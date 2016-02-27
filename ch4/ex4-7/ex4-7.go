1e4edeb938f2e2792a37a1a6acc2941017ec1c90
Derek Miller <Derek701@users.noreply.github.com>
2016-02-27 14:07:52 -0600

    Complete Exercise 4.7
    Modify reverse to reverse characters in place and reduce allocated
    space.

--- ch4/ex4-7/ex4-7.go
+++ ch4/ex4-7/ex4-7.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 	a := []byte("0 I 2 3 4 Π")
@@ -8,11 +11,17 @@ func main() {
 	fmt.Println(string(a)) // "Π 4 3 2 I 0"
 }
 
-// reverse reverses the characters of a slice.
+// reverse reverses a slice of characters in place.
 func reverse(bytes []byte) {
-	runes := []rune(string(bytes))
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	var r1, r2 rune
+	var l1, l2 int
+	for i, j := 0, len(bytes); i < j; i, j = i+l2, j-l1 {
+		r1, l1 = utf8.DecodeRune(bytes[i:])
+		r2, l2 = utf8.DecodeLastRune(bytes[:j])
+		if l2 > l1 {
+			copy(bytes[i+l2:], bytes[i+l1:j-l2])
+		}
+		copy(bytes[i:], []byte(string(r2)))
+		copy(bytes[j-l1:], []byte(string(r1)))
 	}
-	copy(bytes, []byte(string(runes)))
 }
