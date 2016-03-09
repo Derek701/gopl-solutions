297d58e1545bcd22dd9b5179e0ab1376e7a36f81
Derek Miller <Derek701@users.noreply.github.com>
2016-03-09 15:55:31 -0600

    Complete Exercise 4.10
    Modify issues to report the results in age categories of less than 30
    days old, between 30 and 365 days old, and more than 365 days old.

--- ch4/ex4-10/ex4-10.go
+++ ch4/ex4-10/ex4-10.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/Derek701/gopl-solutions/ch4/ex4-10/github"
 )
@@ -15,8 +16,26 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("%d issues:\n", result.TotalCount)
+	now := time.Now()
+	fmt.Println("\nLess than 30 days old...")
 	for _, item := range result.Items {
-		fmt.Printf("#%-5d %9.9s %.55s\n",
-			item.Number, item.User.Login, item.Title)
+		if now.Sub(item.CreatedAt).Hours()/24 < 30 {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				item.Number, item.User.Login, item.Title)
+		}
+	}
+	fmt.Println("\nBetween 30 and 365 days old...")
+	for _, item := range result.Items {
+		if now.Sub(item.CreatedAt).Hours()/24 >= 30 && now.Sub(item.CreatedAt).Hours()/24 <= 365 {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				item.Number, item.User.Login, item.Title)
+		}
+	}
+	fmt.Println("\nMore than 365 days old...")
+	for _, item := range result.Items {
+		if now.Sub(item.CreatedAt).Hours()/24 > 365 {
+			fmt.Printf("#%-5d %9.9s %.55s\n",
+				item.Number, item.User.Login, item.Title)
+		}
 	}
 }
